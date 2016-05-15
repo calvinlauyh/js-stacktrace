e1f8645751595942e9138ac005987b18575ca781
Yu H <yuhlau@gmail.com>
2016-05-15 18:34:24 +0800

    Update copyright notice

--- getStackTrace.js
+++ getStackTrace.js
@@ -2,9 +2,9 @@
  * JS StackTrace
  * Get debug stacktrace in JavaScript
  *
- * Copyright (c) 2016 Lau Yu Hei
+ * Copyright (c) 2016 Yu H.
  * 
- * @author Lau Yu Hei
+ * @author Yu H.
  * @version 1.0.2
  * @license The MIT License (MIT)
  * https://opensource.org/licenses/MIT
